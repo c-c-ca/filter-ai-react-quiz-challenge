023c632d0c5b1961a76795af64eba104361b3c1f
Adam Carter <adam.clayton.carter@gmail.com>
2021-08-13 17:59:22 -0400

    Store questions as json

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,5 @@
 import React, { useState } from "react";
-
-const questions = [
-  {
-    question: "Which is the largest country in the world by population?",
-    options: ["India", "USA", "China", "Russia"],
-    answer: "China",
-  },
-  {
-    question: "When did the second world war end?",
-    options: ["1945", "1939", "1944", "1942"],
-    answer: "1945",
-  },
-  {
-    question: "Which was the first country to issue paper currency?",
-    options: ["USA", "France", "Italy", "China"],
-    answer: "China",
-  },
-  {
-    question: "Which city hosted the 1996 Summer Olympics?",
-    options: ["Atlanta", "Sydney", "Athens", "Beijing"],
-    answer: "Atlanta",
-  },
-  {
-    question: "Who invented the telephone?",
-    options: [
-      "Albert Einstein",
-      "Alexander Graham Bell",
-      "Isaac Newton",
-      "Marie Curie",
-    ],
-    answer: "Alexander Graham Bell",
-  },
-];
+import questions from "../data/questions.json";
 
 const Home = () => {
   const [score, setScore] = useState(0);
